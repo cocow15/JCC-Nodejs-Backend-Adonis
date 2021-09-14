9f875e98727642fe05964881092cd57cacb09d08
cocow15 <79132450+dimasWisnu420@users.noreply.github.com>
2021-09-14 22:42:44 +0700

    Tugas: Membuat Aplikasi REST API CRUD dengan Sequelize

---
+++ Tugas 12 - CRUD Express Sequelize/todo-api/controllers/venue.js
@@ -0,0 +1,65 @@
+//Import model
+const {nilai} = require('../models');
+class venueController{
+    static async showAll(req, res){
+        try {
+            let realData = await nilai.findAll();
+            res.status(200).json({status: 'Success', data: realData})
+        } catch (error) {
+            res.status(401).json({error: error})
+        }
+    }
+
+    static async store(req, res){
+        //tangkap data melalui req
+        let name = req.body.name;
+        let address = req.body.address;
+        let phone = req.body.phone;
+        //simpan data menggunakan id
+        let newVenue = nilai.build({name, address, phone});
+        console.log(newVenue instanceof nilai);
+        await newVenue.save()
+        res.status(200).json({status: 'Success', message: "Data berhasil disimpan"})
+    }
+
+    static async findId(req, res){
+        let id = req.params.id;
+        // let value = await nilai.findAll({
+        //     where: {
+        //         id: id
+        //     }
+        //     limit: 1;
+        // })
+        let value = await nilai.findByPk(id);
+
+        res.status(200).json({status: 'succes', data: value})
+    }
+
+    static async update(req, res){
+        let name = req.body.name;
+        let address = req.body.address;
+        let phone = req.body.phone;
+        let query = await nilai.update({
+            name: name,
+            address: address, 
+            phone: phone
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(200).json({status: 'succes', data: "updated"})
+    }
+
+    static async destroy(req, res){
+        await nilai.destroy({
+            where: {
+                id: req.params.id 
+            }
+        })
+        res.status(200).json({status: 'succes', data: "deleted"})
+    }
+
+}
+
+module.exports = venueController;
